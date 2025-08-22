95a35cac5b1ffa284891b1dfc826ac883bb30ed5
Ronny Korneli <>
2025-08-23 00:24:19 +0200

    changed the sheet layout

--- netlify/functions/sendToSheets.ts
+++ netlify/functions/sendToSheets.ts
@@ -1,7 +1,7 @@
 export async function handler(event) {
   const data = JSON.parse(event.body);
 
-  const response = await fetch("https://script.google.com/macros/s/AKfycbxVhrw4qUAlGFHnySpSbN36XaFiXYKBar9Uk1pWAd1gwuDWvjxUVdXH-yC6qgLlVmL1UA/exec",{
+  const response = await fetch("https://script.google.com/macros/s/AKfycbxg2dyVqdv2LQhf768rC_99itxSpm8HUCIPlcKgqferDQQEJqaKy9RINGovwNhHquU65g/exec",{
     method: "POST",
     body: JSON.stringify(data),
     headers: { "Content-Type": "application/json" },
