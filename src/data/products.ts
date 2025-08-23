53a961e0d651469d66b141138bc411c39fa904c1
Ronny Korneli <>
2025-08-23 09:29:30 +0200

    changed the product array names

--- src/data/products.ts
+++ src/data/products.ts
@@ -1,73 +1,73 @@
 
 export const products = [
   {
-    id: "vacuum-cleaner-bags",
+    id: "vacuumCleanerBags",
     name: "Vacuum cleaner bags",
     label: "Vacuum cleaner bags",
     forOnLabel: "",
     placeholderName: "Menge eintragen"
   },
   {
-    id: "laundry-detergent-sheets",
+    id: "laundryDetergentSheets",
     name: "Laundry Detergent Sheets",
     label: "Laundry detergent (sheets)",
     forOnLabel: "",
     placeholderName: "Menge eintragen"
   },
   {
-    id: "hand-soap-refillable",
+    id: "handSoapRefillable",
     name: "Hand Soap Refillable",
     label: "Hand soap (refillable)",
     forOnLabel: "hand-soap-refillable",
     placeholderName: "Menge eintragen"
   },
   {
-    id: "scouring-milk",
+    id: "scouringMilk",
     name: "Milk / Cream Cleanser",
     label: "Scouring milk/cream cleanser",
     forOnLabel: "scouring-milk",
     placeholderName: "Menge eintragen"
   },
   {
-    id: "folded-paper-towels",
+    id: "foldedPaperTowels",
     name: "Folded Paper Towels",
     label: "Folded paper towels",
     forOnLabel: "folded-paper-towels",
     placeholderName: "Menge eintragen"
   },
   {
-    id: "toilet-paper",
+    id: "toiletPaper",
     name: "Toilet Paper",
     label: "Toilet Paper",
     forOnLabel: "toilet-paper",
     placeholderName: "Menge eintragen"
   },
   {
-    id: "disinfectant-hands",
+    id: "disinfectantHands",
     name: "Disinfectant Hands",
     label: "Disinfectant (hands)",
     forOnLabel: "disinfectant-hands",
     placeholderName: "Menge eintragen"
   },
   {
-    id: "red-cleaning-agent",
+    id: "redCleaningAgent",
     name: "Red Cleaning Agent",
     label: "Red cleaning agent",
     forOnLabel: "red-cleaning-agent",
     placeholderName: "Menge eintragen"
   },
   {
-    id: "blue-cleaning-agent",
+    id: "blueCleaningAgent",
     name: "Blue Cleaning Agent",
     label: "Blue cleaning agent",
     forOnLabel: "blue-cleaning-agent",
     placeholderName: "Menge eintragen"
   },
   {
-    id: "disinfectant-wipes-1",
-    name: "Disinfectant Wipes1",
+    id: "disinfectantWipes",
+    name: "Disinfectant Wipes",
     label: "Disinfectant wipes",
-    forOnLabel: "disinfectant-wipes-1",
+    forOnLabel: "disinfectant-wipes",
     placeholderName: "Menge eintragen"
   }
 ];
