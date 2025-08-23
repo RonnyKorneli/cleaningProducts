3ccbed09c184f7ed05ec6c4388c5ed5e041efd4c
Ronny Korneli <>
2025-08-23 08:59:48 +0200

    updated the url for google sheets

--- netlify/functions/sendToSheets.ts
+++ netlify/functions/sendToSheets.ts
@@ -1,7 +1,7 @@
 export async function handler(event) {
   const data = JSON.parse(event.body);
 
-  const response = await fetch("https://script.google.com/macros/s/AKfycbxg2dyVqdv2LQhf768rC_99itxSpm8HUCIPlcKgqferDQQEJqaKy9RINGovwNhHquU65g/exec",{
+  const response = await fetch("https://script.google.com/macros/s/AKfycbx3UGxsrLATHLIJc5zXHmiWANCLRrBXbwlsOqLOzfJ8m0uWnYhD0UlHmbVRsgKyCbXUBQ/exec",{
     method: "POST",
     body: JSON.stringify(data),
     headers: { "Content-Type": "application/json" },
