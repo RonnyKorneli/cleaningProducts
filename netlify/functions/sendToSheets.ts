03dcaa9c1f07d07e0ee5ad93adc1de540a5daef6
Ronny Korneli <>
2025-08-23 09:19:28 +0200

    chnaged url google sheets

--- netlify/functions/sendToSheets.ts
+++ netlify/functions/sendToSheets.ts
@@ -1,7 +1,7 @@
 export async function handler(event) {
   const data = JSON.parse(event.body);
 
-  const response = await fetch("https://script.google.com/macros/s/AKfycbx3UGxsrLATHLIJc5zXHmiWANCLRrBXbwlsOqLOzfJ8m0uWnYhD0UlHmbVRsgKyCbXUBQ/exec",{
+  const response = await fetch("https://script.google.com/macros/s/AKfycbz2cTJfFVoyC2ruvcRYeeAEidM_Uo-DeYWe5Yoxg2_pkYSjM9MiTUAUg0bfb7Lv4OUa7A/exec",{
     method: "POST",
     body: JSON.stringify(data),
     headers: { "Content-Type": "application/json" },
