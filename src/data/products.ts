0b8a13825f77a3898716baabb5ba8a719aea2406
Ronny Korneli <>
2025-08-23 08:40:25 +0200

    refactored input fields

---
+++ src/data/products.ts
@@ -0,0 +1,73 @@
+
+export const products = [
+  {
+    id: "vacuum-cleaner-bags",
+    name: "Vacuum cleaner bags",
+    label: "Vacuum cleaner bags",
+    forOnLabel: "",
+    placeholderName: "Menge eintragen"
+  },
+  {
+    id: "laundry-detergent-sheets",
+    name: "Laundry Detergent Sheets",
+    label: "Laundry detergent (sheets)",
+    forOnLabel: "",
+    placeholderName: "Menge eintragen"
+  },
+  {
+    id: "hand-soap-refillable",
+    name: "Hand Soap Refillable",
+    label: "Hand soap (refillable)",
+    forOnLabel: "hand-soap-refillable",
+    placeholderName: "Menge eintragen"
+  },
+  {
+    id: "scouring-milk",
+    name: "Milk / Cream Cleanser",
+    label: "Scouring milk/cream cleanser",
+    forOnLabel: "scouring-milk",
+    placeholderName: "Menge eintragen"
+  },
+  {
+    id: "folded-paper-towels",
+    name: "Folded Paper Towels",
+    label: "Folded paper towels",
+    forOnLabel: "folded-paper-towels",
+    placeholderName: "Menge eintragen"
+  },
+  {
+    id: "toilet-paper",
+    name: "Toilet Paper",
+    label: "Toilet Paper",
+    forOnLabel: "toilet-paper",
+    placeholderName: "Menge eintragen"
+  },
+  {
+    id: "disinfectant-hands",
+    name: "Disinfectant Hands",
+    label: "Disinfectant (hands)",
+    forOnLabel: "disinfectant-hands",
+    placeholderName: "Menge eintragen"
+  },
+  {
+    id: "red-cleaning-agent",
+    name: "Red Cleaning Agent",
+    label: "Red cleaning agent",
+    forOnLabel: "red-cleaning-agent",
+    placeholderName: "Menge eintragen"
+  },
+  {
+    id: "blue-cleaning-agent",
+    name: "Blue Cleaning Agent",
+    label: "Blue cleaning agent",
+    forOnLabel: "blue-cleaning-agent",
+    placeholderName: "Menge eintragen"
+  },
+  {
+    id: "disinfectant-wipes-1",
+    name: "Disinfectant Wipes1",
+    label: "Disinfectant wipes",
+    forOnLabel: "disinfectant-wipes-1",
+    placeholderName: "Menge eintragen"
+  }
+];
