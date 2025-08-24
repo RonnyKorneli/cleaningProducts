36723dd3d22fe585a6834917c1f53eddca22fc92
Ronny Korneli <>
2025-08-24 10:45:39 +0200

    update google sheet url

--- netlify/functions/sendToSheets.ts
+++ netlify/functions/sendToSheets.ts
@@ -1,7 +1,7 @@
 export async function handler(event) {
   const data = JSON.parse(event.body);
 
-  const response = await fetch("https://script.google.com/macros/s/AKfycbyEfGbXNXsiADdd_b6RgDlpsokThNm9W6zCQH7UeIFTLe_v-YNuJU8gHnl8-AmKiw3pXA/exec",{
+  const response = await fetch("https://script.google.com/macros/s/AKfycbwjqQZqV6hMvg5Xls24RKZlG93v0NA17eGFqxovFIIQqq78P8cgaCbKwNLN1p710uowIg/exec",{
     method: "POST",
     body: JSON.stringify(data),
     headers: { "Content-Type": "application/json" },
