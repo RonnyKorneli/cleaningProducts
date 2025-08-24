75a8d013c681afc682211b5750e6b1b97611a66a
Ronny Korneli <>
2025-08-24 13:00:50 +0200

    connected to anoutger google account

--- netlify/functions/sendToSheets.ts
+++ netlify/functions/sendToSheets.ts
@@ -1,7 +1,7 @@
 export async function handler(event) {
   const data = JSON.parse(event.body);
 
-  const response = await fetch("https://script.google.com/macros/s/AKfycbwjqQZqV6hMvg5Xls24RKZlG93v0NA17eGFqxovFIIQqq78P8cgaCbKwNLN1p710uowIg/exec",{
+  const response = await fetch("https://script.google.com/macros/s/AKfycbxDcZeKfT0xGWERduxU0UBatp4mflZTndhQyDL_rYT_38xNade92sdrNoYVyHFBhTTvYw/exec",{
     method: "POST",
     body: JSON.stringify(data),
     headers: { "Content-Type": "application/json" },
