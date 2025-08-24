c2a0d7a913e513f50ab6879c2c5f911e7d4932c3
Ronny Korneli <>
2025-08-24 13:54:49 +0200

    chenged the labels slighty of products

--- src/data/products.ts
+++ src/data/products.ts
@@ -3,69 +3,69 @@ export const products = [
   {
     id: "vacuumCleanerBags",
     name: "Vacuum cleaner bags",
-    label: "Vacuum cleaner bags",
+    label: "Vacuum cleaner bags (Number of Bags still there)",
     forOnLabel: "",
     placeholderName: "Menge eintragen"
   },
   {
     id: "laundryDetergentSheets",
     name: "Laundry Detergent Sheets",
-    label: "Laundry detergent (sheets)",
+    label: "Laundry detergent (Number of packages still there)",
     forOnLabel: "",
     placeholderName: "Menge eintragen"
   },
   {
     id: "handSoapRefillable",
     name: "Hand Soap Refillable",
-    label: "Hand soap (refillable)",
+    label: "Hand soap refillable (Number of bottles still there)",
     forOnLabel: "hand-soap-refillable",
     placeholderName: "Menge eintragen"
   },
   {
     id: "scouringMilk",
     name: "Milk / Cream Cleanser",
-    label: "Scouring milk/cream cleanser",
+    label: "Scouring milk/cream cleanser (Number of small packs)",
     forOnLabel: "scouring-milk",
     placeholderName: "Menge eintragen"
   },
   {
     id: "foldedPaperTowels",
     name: "Folded Paper Towels",
-    label: "Folded paper towels",
+    label: "Folded paper towels (Number of full packs still there)",
     forOnLabel: "folded-paper-towels",
     placeholderName: "Menge eintragen"
   },
   {
     id: "toiletPaper",
     name: "Toilet Paper",
-    label: "Toilet Paper",
+    label: "Toilet Paper (Number of Rolls still there)",
     forOnLabel: "toilet-paper",
     placeholderName: "Menge eintragen"
   },
   {
     id: "disinfectantHands",
     name: "Disinfectant Hands",
-    label: "Disinfectant (hands)",
+    label: "Disinfectant for hands (Number of full bottles)",
     forOnLabel: "disinfectant-hands",
     placeholderName: "Menge eintragen"
   },
   {
     id: "redCleaningAgent",
     name: "Red Cleaning Agent",
-    label: "Red cleaning agent",
+    label: "Red cleaning agent (Number of full bottles)",
     forOnLabel: "red-cleaning-agent",
     placeholderName: "Menge eintragen"
   },
   {
     id: "blueCleaningAgent",
     name: "Blue Cleaning Agent",
-    label: "Blue cleaning agent",
+    label: "Blue cleaning agent (Number of full bottles)",
     forOnLabel: "blue-cleaning-agent",
     placeholderName: "Menge eintragen"
   },
   {
     id: "disinfectantWipes",
-    name: "Disinfectant Wipes",
+    name: "Disinfectant Wipes (Number of full packages)",
     label: "Disinfectant wipes",
     forOnLabel: "disinfectant-wipes",
     placeholderName: "Menge eintragen"
