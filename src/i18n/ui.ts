31175932901c39859962be2d6e27faacc84e6dc6
Ronny Korneli <>
2025-09-05 15:19:53 +0200

    added translation to product list headline

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -11,15 +11,20 @@ export const ui = {
   de: {
     'home.headline':'Reinigungsprodukte',
     'home.chooseKita':'Wähle eine Kita',
+    'productList.whatWeHaveStorage':'Was haben wir im Lager?',
+
   },
   en: {
     'home.headline':'Cleaning Products',
     'home.chooseKita':'Choose a Kita',
+    'productList.whatWeHaveStorage':'What do we have in Storage?',
+
 
   },
   no:{
     'home.headline':'Rengjøringsprodukter',
     'home.chooseKita':'Velg en barnehage',
+    'productList.whatWeHaveStorage':'Hva har vi på lager?',
 
 
   }
