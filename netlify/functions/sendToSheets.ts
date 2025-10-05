dc82b36598d98fa27f7fed474b0bff785eb3a830
Ronny Korneli <>
2025-10-05 10:58:43 +0200

    added more new url to google sheets

--- netlify/functions/sendToSheets.ts
+++ netlify/functions/sendToSheets.ts
@@ -1,7 +1,7 @@
 export async function handler(event) {
   const data = JSON.parse(event.body);
 
-  const response = await fetch("https://script.google.com/macros/s/AKfycbybG65gfi3TS_ahByxGe67rEdc97sH_6vWZ9TxkVUDw3X_ryLih2Zhl_eBS_8Tfxci1-Q/exec",{
+  const response = await fetch("https://script.google.com/macros/s/AKfycbyqRbtAq9Y9yzXmoav-cIztWqWhAuVnACbjbqZOUpGr_7TNhsKC9pjCzRiBehpQCmMYIA/exec",{
     method: "POST",
     body: JSON.stringify(data),
     headers: { "Content-Type": "application/json" },
