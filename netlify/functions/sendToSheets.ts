aa1460440f60c8fd99427eccef96b8ea3ff6e5c9
Ronny Korneli <>
2025-10-05 09:41:12 +0200

    made changes to the headers on google sheets

--- netlify/functions/sendToSheets.ts
+++ netlify/functions/sendToSheets.ts
@@ -1,7 +1,7 @@
 export async function handler(event) {
   const data = JSON.parse(event.body);
 
-  const response = await fetch("https://script.google.com/macros/s/AKfycbz262z0kxv0JUeY2fwD2TRxTDG3ahYzh5ZCV5EU3rOdwDxK3MGUyXGJqcqFxdJA2DjIsg/exec",{
+  const response = await fetch("https://script.google.com/macros/s/AKfycbzKh16eesZcdrSZBW3eM9V9BCh_md6VRe6jnXraveDKupVTIKsseL1E8eQBZY9vgSl3kw/exec",{
     method: "POST",
     body: JSON.stringify(data),
     headers: { "Content-Type": "application/json" },
